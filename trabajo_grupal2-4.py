7664112dcd5dc737a7afbb8e3d0066e7445f4e75
rayssa-ale <alejofloresrayssa@gmail.com>
2025-09-11 09:27:09 -0300

    Update trabajo_grupal2-4.py
    realice cambios en el codigo

--- trabajo_grupal2-4.py
+++ trabajo_grupal2-4.py
@@ -1,13 +1,29 @@
-numeros = list(map(int, input("Ingresa números separados por espacio: ").split()))
+filas = int(input("Ingresa cantidad de filas: "))
+columnas = int(input("Ingresa cantidad de columnas: "))
 
-pares = 0
-impares = 0
+matriz = []
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        valor = int(input(f"Ingrese elemento [{i}][{j}]: "))
+        fila.append(valor)
+    matriz.append(fila)
 
-for n in numeros:
-    if n % 2 == 0:
-        pares = pares + 1
-    else:
-        impares = impares + 1
+rotada = []
+for i in range(columnas):
+    fila_rotada = []
+    for j in range(filas):
+        fila_rotada.append(0)
+    rotada.append(fila_rotada)
 
-print("Cantidad de pares:", pares)
-print("Cantidad de impares:", impares)
+for i in range(filas):
+    for j in range(columnas):
+        rotada[j][filas - 1 - i] = matriz[i][j]
+
+print("\nMatriz original:")
+for fila in matriz:
+    print(fila)
+
+print("\nMatriz rotada 90°:")
+for fila in rotada:
+    print(fila)
